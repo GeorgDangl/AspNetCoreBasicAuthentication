9438d9d42f6809b9f94180ab5bcbd102021340ed
Georg Dangl <georg@dangl.me>
2017-11-09 00:29:26 +0100

    Add authentication configuration

--- AspNetCoreBasicAuthentication/Startup.cs
+++ AspNetCoreBasicAuthentication/Startup.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -10,14 +7,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using AspNetCoreBasicAuthentication.Data;
 using AspNetCoreBasicAuthentication.Models;
+using AspNetCoreBasicAuthentication.Authentication;
 using AspNetCoreBasicAuthentication.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace AspNetCoreBasicAuthentication
 {
     public class Startup
     {
         private static readonly string _inMemoryDbName = Guid.NewGuid().ToString();
-        
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -31,14 +31,35 @@ namespace AspNetCoreBasicAuthentication
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseInMemoryDatabase(_inMemoryDbName));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+            services.AddIdentity<ApplicationUser, IdentityRole>(c =>
+                {
+                    c.Password.RequireDigit = false;
+                    c.Password.RequiredLength = 3;
+                    c.Password.RequireLowercase = false;
+                    c.Password.RequireNonAlphanumeric = false;
+                    c.Password.RequireUppercase = false;
+                })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
-            services.AddMvc();
+            services.AddAuthentication(c =>
+                {
+                    c.DefaultAuthenticateScheme = "Basic";
+                    c.DefaultChallengeScheme = "Basic"; // Default to basic prevents cookie auth redirect to login on 401
+                })
+                .AddHttpBasicAuthentication();
+
+            services.AddMvc(options =>
+            {
+                // The default policy is to make sure that both authentication schemes - Cookie and Http Basic - are challenged
+                var defaultPolicy = new AuthorizationPolicyBuilder(IdentityConstants.ApplicationScheme, "Basic")
+                    .RequireAssertion(c => true) // A requirement is mandatory
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(defaultPolicy));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
