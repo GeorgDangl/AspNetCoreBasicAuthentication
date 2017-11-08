553a27a5475eedb6261f53494ea4099db8085396
Georg Dangl <georg@dangl.me>
2017-11-09 00:29:38 +0100

    Add protected api endpoint

---
+++ AspNetCoreBasicAuthentication/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBasicAuthentication.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    public class ValuesController : Controller
+    {
+        public IActionResult Values()
+        {
+            var values = new[] {"You", "are", "awesome!"};
+            return Ok(values);
+        }
+    }
+}
