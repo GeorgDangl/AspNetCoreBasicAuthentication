70acd60524a28c9cb45286ad5c6fdd358d1df784
Georg Dangl <georg@dangl.me>
2017-11-09 00:26:37 +0100

    Add initialization at app start

--- AspNetCoreBasicAuthentication/Program.cs
+++ AspNetCoreBasicAuthentication/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreBasicAuthentication.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace AspNetCoreBasicAuthentication
@@ -14,7 +17,15 @@ namespace AspNetCoreBasicAuthentication
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                DatabaseInitializer.InitializeDatabase(serviceScope)
+                    .ConfigureAwait(false)
+                    .GetAwaiter()
+                    .GetResult();
+            }
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
